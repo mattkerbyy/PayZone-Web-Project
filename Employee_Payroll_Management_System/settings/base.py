159a7201f2a76fefcbce46a2d00e789bb69b0a2e
mattkerbyy <mattkerbyy@gmail.com>
2023-11-14 05:20:17 +0800

    11/14/2023

--- Employee_Payroll_Management_System/settings/base.py
+++ Employee_Payroll_Management_System/settings/base.py
@@ -131,3 +131,13 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 #
 # # Redirect to this URL after successful login
 # LOGIN_REDIRECT_URL = 'PayZone:dashboard'
+
+CORS_REPLACE_HTTPS_REFERER      = False
+HOST_SCHEME                     = "http://"
+SECURE_PROXY_SSL_HEADER         = None
+SECURE_SSL_REDIRECT             = False
+SESSION_COOKIE_SECURE           = False
+CSRF_COOKIE_SECURE              = False
+SECURE_HSTS_SECONDS             = None
+SECURE_HSTS_INCLUDE_SUBDOMAINS  = False
+SECURE_FRAME_DENY               = False
